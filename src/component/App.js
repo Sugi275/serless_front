86949548ac431fdb715815f7dcb362a31bb28139
sugimount <sugi.mount@gmail.com>
2019-10-13 08:54:25 +0000

    refact

--- src/component/App.js
+++ src/component/App.js
@@ -5,17 +5,31 @@ import ComplexGrid from './complexgrid'
 import ImageUpload from './imageupload'
 import PropTypes from 'prop-types'
 
-const App = ({ history }) => {
-  return (
-    <React.Fragment>
-      <AppBar />
-      <Switch>
-        <Route exact path="/" component={ComplexGrid} />
-        <Route exact path="/imageupload" component={ImageUpload} />
-      </Switch>
-    </React.Fragment>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <AppBar />
+        <Switch>
+          <Route exact path="/" component={ComplexGrid} />
+          <Route exact path="/imageupload" component={ImageUpload} />
+        </Switch>
+      </React.Fragment>
+    )
+  }
 }
+
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <AppBar />
+//       <Switch>
+//         <Route exact path="/" component={ComplexGrid} />
+//         <Route exact path="/imageupload" component={ImageUpload} />
+//       </Switch>
+//     </React.Fragment>
+//   )
+// }
 
 App.propTypes = {
   history: PropTypes.object,
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,12 @@ import { connect } from 'react-redux'
 import App from '../component/App'
 
 // store から必要なstateを取り出して、Componentのpropsに紐づける関数
-function mapStateToProps(state) {
-    return state
-}
+const mapStateToProps = state => ({
+    state: state
+})
 
 // Action の Dispatch を行う処理をこの関数内に閉じる。コンポーネントから dispatch を隠蔽
-function mapDispatchToProps(dispatch) {
-    return {
-    }
-}
+const mapDispatchToProps = dispatch => ({
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
