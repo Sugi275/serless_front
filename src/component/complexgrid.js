83c59f36dba0638d489eda91f50402f3684e7e3c
sugimount <sugi.mount@gmail.com>
2019-10-13 18:42:19 +0000

    横並びダン

--- src/component/complexgrid.js
+++ src/component/complexgrid.js
@@ -4,6 +4,7 @@ import { Button } from '@material-ui/core';
 import store from '../store/configureStore';
 import GetImageMetadata from '../oraclecloud/getImageMetadata';
 import ImageCard from './ImageCard'
+import './complexgrid.css'
 
 export default class ComplexGrid extends React.Component {
   debugReloadImages() {
@@ -34,12 +35,50 @@ export default class ComplexGrid extends React.Component {
     }
 
     return (
-      <div>
+      <React.Fragment>
         <Button onClick={() => { this.handleReloadImages() }}>
           ReloadImages
         </Button>
-        {list}
-      </div>
+        <div className="flex">
+          {list}
+        </div>
+      </React.Fragment >
     )
+
+    // return (
+    //   <div>
+    //     <Button onClick={() => { this.handleReloadImages() }}>
+    //       ReloadImages
+    //     </Button>
+    //     <div style={{ display: 'table' }}>
+    //       <div style={{ display: 'table-cell' }}>1</div>
+    //       <div style={{ display: 'table-cell' }}>2</div>
+    //       <div style={{ display: 'table-cell' }}>3</div>
+    //     </div>
+    //     <div style={{ display: 'table' }}>
+    //       <div style={{ display: 'table-cell' }}>4</div>
+    //       <div style={{ display: 'table-cell' }}>5</div>
+    //       <div style={{ display: 'table-cell' }}>6</div>
+    //     </div>
+    //   </div>
+    // )
+
+    // return (
+    //   <div>
+    //     <Button onClick={() => { this.handleReloadImages() }}>
+    //       ReloadImages
+    //     </Button>
+    //     <div className="block">
+    //       <div className="block_element">1</div>
+    //       <div className="block_element">2</div>
+    //       <div className="block_element">3</div>
+    //     </div>
+    //     <div className="block">
+    //       <div className="block_element">4</div>
+    //       <div className="block_element">5</div>
+    //       <div className="block_element">6</div>
+    //     </div>
+    //   </div>
+    // )
   }
 }
